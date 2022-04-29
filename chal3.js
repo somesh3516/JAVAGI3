b15237a1c928dec32434a352e6865519aada5cba
somesh3516 <102987739+somesh3516@users.noreply.github.com>
2022-04-29 14:52:24 -0400

    done

---
+++ chal3.js
@@ -0,0 +1,22 @@
+//const myFunction= (num1,num2) => 
+//     num1+num2
+//};
+
+
+//arrowAdd100=(money) =>money+100;
+
+
+
+let fun= (x,y)=>{
+    fun("hello my name is somesh!","e")
+    let count=0;
+    for(let i=0; i < x.length; i++){
+        if(x[i]==y){
+            count+=1;
+        }
+         
+    return count
+
+}
+}
+console.log(count)
